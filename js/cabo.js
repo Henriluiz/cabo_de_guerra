d6caefee37ca6c6f2ab7f5bd274a2192e8838651
Luiz Azevedo <luizao.lids@gmail.com>
2025-04-04 10:28:07 -0300

    v0.1 iniciando projeto

---
+++ js/cabo.js
@@ -0,0 +1,25 @@
+// Um jogo de Escolha de Time e Disputa no Cabo de Guerra. Cada personagem/jogador terá uma força já pré-definida, ao escolher 3 jogadores, a máquina escolherá outros 3 (Alguns igual ou não) e somará a força dos três integrantes do time e o time que tive a maior força irá ganhar
+
+// Lista de jogadores
+const heroisMarvel = {
+    "Capitão América": 800,
+    "Thor": 1000,
+    "Homem de Ferro": 700,
+    "Homem Aranha": 750,
+    "Hulk": 999,
+  };
+
+const viloesMarvel = {
+    "Caveira Vermelha": 790,
+    "Fantasmas": 720,
+    "Chicote Negro": 850,
+    "Thanos": 1000,
+    "Ultron": 950,
+    "Soldado Invernal": 820,
+};
+  
+// Calcular a força de cada jogador, e depois somar pra saber a força geral do time
+
+// Calcular a força de cada jogador do time do computador
+
+// Comparar os dois times para saber quem ganhou!
